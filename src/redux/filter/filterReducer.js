4581a68802b555bd9ede655d1db33ca81022e5f1
UshakovaNataliia <65922563+UshakovaNataliia@users.noreply.github.com>
2020-12-14 17:51:09 +0200

    sum

--- src/redux/filter/filterReducer.js
+++ src/redux/filter/filterReducer.js
@@ -1,6 +1,16 @@
-import { filterInput } from './filterActions';
-import { createReducer } from '@reduxjs/toolkit';
+import {FILTER_CONTACT} from './filterActionsTypes';
 
+<<<<<<< Updated upstream
 export const filter = createReducer('', {
     [filterInput]: (_, action) => action.payload, // _ - state не используется
 });
+=======
+export const filter = (state = '', {type, payload}) => {
+    switch (type) {
+        case FILTER_CONTACT:
+            return payload.filter;
+        default:
+            return state;
+    }
+};
+>>>>>>> Stashed changes
