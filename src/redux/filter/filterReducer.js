b0f2664b7ccbae8555203776a003125e5604d3c9
UshakovaNataliia <65922563+UshakovaNataliia@users.noreply.github.com>
2020-12-14 18:32:18 +0200

    +

--- src/redux/filter/filterReducer.js
+++ src/redux/filter/filterReducer.js
@@ -1,8 +1,5 @@
 import {FILTER_CONTACT} from './filterActionsTypes';
 
-export const filter = createReducer('', {
-    [filterInput]: (_, action) => action.payload, // _ - state не используется
-});
 export const filter = (state = '', {type, payload}) => {
     switch (type) {
         case FILTER_CONTACT:
